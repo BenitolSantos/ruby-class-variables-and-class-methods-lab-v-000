4cc84cedbb9b7a177684d23d35603a2a26f2642f
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-26 16:50:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,9 @@ class Song
   attr_accessor :artist,:name, :genre #attr_accessor -< no : immediately after
   @@count = 0
 
+  def new(name,artist,genre)
+  end
+
   def initialize(name)
     @name = name
     @@count += 1
